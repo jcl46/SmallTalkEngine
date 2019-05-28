40e0333f00773c12d451e763a955c44669cde0a8
Anton Rosenoern <antonr@live.dk>
2019-05-28 13:48:47 +0200

    ...

--- SmallTalkEngine/Headers/SmallTalkEngine.h
+++ SmallTalkEngine/Headers/SmallTalkEngine.h
@@ -1,1 +1,35 @@
-﻿
+﻿class SmallTalkEngine
+{
+	public:
+		class Point2D
+		{
+		public:
+			float X;
+			float Y;
+			Point2D(float x, float y)
+			{
+				X = x;
+				Y = y;
+			}
+		};
+		class Vector2D
+		{
+		public:
+			float X;
+			float Y;
+			float Velocity;
+			Vector2D(float x, float y, float velocity)
+			{
+				X = x;
+				Y = y;
+				Velocity = velocity;
+			}
+			Vector2D(float x, float y)
+			{
+				X = x;
+				Y = y;
+				Velocity = 0;
+			}
+		};
+		class
+};
